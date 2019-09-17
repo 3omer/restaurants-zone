58db732022352af45e0fd60edb4236e7ad2043f6
OMAR <omo9@mail.com>
2019-09-18 02:57:40 +0300

    refactor form_handler clasess

--- forms_handler.py
+++ forms_handler.py
@@ -5,19 +5,74 @@ class _Input:
         self.value = value
         self.errors = errors
 
-class ItemFormHandler:
+
+class FormHandler:
+    '''Abstract class defining methods neccessary to validate forms. \
+        form input represented by the Input class. where Input.value is the input value returned from form, errors messages must be set by input validators.
+        subclasses mut call super constructor
+    '''
+    def __init__(self):
+        self.is_valid = False
+    
+    @property
+    def input_args(self):
+        ''' this method return valid (modified if there ) from input as a dict object.\
+            eg. 
+            retutn { 'input-name': Input.value.upper() }
+            note: subclass might check is_valid attribute before returning from this method.
+        '''
+        # TODO  
+        # 
+        pass
+    
+
+    def validate(self):
+        '''This method should check all inputs validators, Return True if all inputss are valid , False other wise, note: invalid failds should be reseted to empty\
+           update self.valid then return 
+        '''
+        return False    
+
+
+
+class RestaurantFormHandler(FormHandler):
+    def __init__(self, name=None):
+        super().__init__()
+        self.name = _Input(name)
+
+    @property
+    def input_arg(self):
+        return {
+            'name': self.name.value
+        }
+
+    def _validate_name(self):
+        if self.name.value and len(self.name.value) > 2:
+            logging.info('restaurant name is valid')
+            return True
+        else:
+            self.name.value = ""
+            self.name.errors = 'Enter a restaurant name. ( 3 letters minimum )'
+            logging.info('invalid restaurant name. value:{}'.format(self.name.value))
+            return False
+    
+    def validate(self):
+        self.is_valid = self._validate_name()
+        return self.is_valid
+
+class ItemFormHandler(FormHandler):
     '''Handle Menu Item form data, provid validation with appropiate error message for invalid fields.\
         pass menu item argument then call validate.
         '''
     def __init__(self, name=None, description=None, course=None, price=None, **kwargs):
+        super().__init__()
         # attributes represents input fields
         # Input object hold value and errors of an input field form
         # the first part of the tupel is the field value 
         # the second is field error messages if any
-        self.name = _Input(name, None)
-        self.description = _Input(description, None)
-        self.course = _Input(course, None)
-        self.price = _Input(price, None)
+        self.name = _Input(name)
+        self.description = _Input(description)
+        self.course = _Input(course)
+        self.price = _Input(price)
 
     @property
     def input_args(self):
@@ -45,7 +100,7 @@ class ItemFormHandler:
             return False
     
     def _validate_course(self):
-        if self.course and len(self.course.value) > 2:   
+        if self.course.value and len(self.course.value) > 2:   
             logging.info('course input is valid')
             return True
         else: 
@@ -78,8 +133,8 @@ class ItemFormHandler:
     
 
     def validate(self):
-        "Return True if all fields are valid , False other wise, note: invalid failds are rested to empty"
+        "Return True if all fields are valid , False other wise, note: invalid failds are reseted to empty"
         # using and will prvent callling all methods when one fail
-        is_valid = self._validate_name() & self._validate_description() \
+        self.is_valid= self._validate_name() & self._validate_description() \
                     & self._validate_course() & self._validate_price()
-        return is_valid     
+        return self.is_valid     
