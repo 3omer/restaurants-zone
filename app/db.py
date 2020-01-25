3c33681aa577a1a5a29637a22128e62ab9e0dc66
OMAR <omo9@mail.com>
2020-01-26 01:58:06 +0300

    fixes imports

---
+++ app/db.py
@@ -0,0 +1,45 @@
+from sqlalchemy import engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship, Session
+from sqlalchemy.sql import func
+from flask import current_app, g
+from flask.cli import click
+from flask.cli import with_appcontext
+
+def get_engine():
+    return create_engine('sqlite:///' + current_app.config['DATABASE'])
+
+
+def get_db():
+    if 'db' not in g:
+        db = Session(get_engine())
+        g.db = db
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    from app.model import Base
+    engine = get_engine()
+    Base.metadata.bind = engine
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Database initialized.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
